50a87efcf31155c5b9d6a3f4ec6a74909b172859
QATHeuzim <ribeiromatheus212@gmail.com>
2022-04-06 13:05:14 -0300

    first commit

---
+++ Projeto subir para git hub/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Então se adianta, tu tá interado\n" +
+                "Que o nego é considerado\n" +
+                "E anda com vários do seu lado\n" +
+                "Da Oeste pra Zona Sul\n" +
+                "É o terror do Pirambu\n" +
+                "Veio pra arrastar todo mundo");
+    }
+}
